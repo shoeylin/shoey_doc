f807e8676ffb2eeec3f7413fad59793d8d3a2dd4
shoeylin <shoeylin@gmail.com>
2018-12-26 17:53:24 +0800

    [todo]leecode 0010-2 add

--- golang/LeetCode-learning/0010.regular-expression-matching/regular-expression-matching.go
+++ golang/LeetCode-learning/0010.regular-expression-matching/regular-expression-matching.go
@@ -17,6 +17,32 @@ func isMatch(s, p string) bool {
 		所以 需要把相應的dp設置成 true
 	*/
 	for j := 1; j < pSize && dp[0][j-1]; j += 2 {
-		if p[j]=='*'
+		if p[j] == '*' {
+			dp[0][j+1] = true
+		}
 	}
+
+	for i := 0; i < sSize; i++ {
+		for j := 0; j < sSize; j++ {
+			if p[j] == '.' || p[j] == s[i] {
+				//p[j]與 s[i]可以匹配上 所以 只要前面匹配 這裡就能匹配上
+				dp[i+1][j+1] = dp[i][j]
+			} else if p[j] == '*' {
+				//此時 p[j]的匹配情況與 p[j-1]的內容相關
+				if p[j-1] != s[i] && p[j-1] != '.' {
+					//p[j]無法與 s[i]匹配上
+					//p[j-1:j+1] 只能當作""
+					dp[i+1][j+1] = dp[i+1][j-1]
+				} else {
+					//p[j]與s[i]匹配上
+					//p[j-1;j+1]作為"x*", 可以有三種解釋
+					dp[i+1][j+1] = dp[i+1][j-1] || // "x*"解釋為 ""
+						dp[i+1][j] || // "x*"解釋為 "x"
+						dp[i][j+1] // "x*"解釋為 "xxx..."
+				}
+			}
+		}
+	}
+
+	return dp[sSize][pSize]
 }
---
+++ golang/LeetCode-learning/0010.regular-expression-matching/regular-expression-matching_test.go
@@ -0,0 +1,51 @@
+package problem0010
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type para struct {
+	one string
+	two string
+}
+
+type ans struct {
+	one bool
+}
+
+type question struct {
+	p para
+	a ans
+}
+
+func Test_OK(t *testing.T) {
+	ast := assert.New(t)
+
+	qs := []question{
+		question{
+			p: para{
+				one: "aab",
+				two: "a*b*c",
+			},
+			a: ans{
+				one: true,
+			},
+		},
+		question{
+			p: para{
+				one: "aa",
+				two: ".*",
+			},
+			a: ans{
+				one: true,
+			},
+		},
+	}
+
+	for _, q := range qs {
+		a, p := q.a, q.p
+		ast.Equal(a.one, isMatch(p.one, p.two), "輸入:%v", p)
+	}
+}
